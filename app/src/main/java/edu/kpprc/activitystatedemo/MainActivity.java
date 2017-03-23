529c35fd2a85ac77fd22771673f563f9d10f67b8
scwang <scwang@mail.nsysu.edu.tw>
2017-03-23 15:28:06 +0800

    init

---
+++ app/src/main/java/edu/kpprc/activitystatedemo/MainActivity.java
@@ -0,0 +1,45 @@
+package edu.kpprc.activitystatedemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    String tag = "LifeCycleEvents";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(tag, "In the onCreate() event");
+    }
+    public void onStart()
+    {
+        super.onStart();
+        Log.d(tag, "In the onStart() event");
+    }
+    public void onRestart()
+    {
+        super.onRestart();
+        Log.d(tag, "In the onRestart() event");
+    }
+    public void onResume()
+    {
+        super.onResume();
+        Log.d(tag, "In the onResume() event");
+    }
+    public void onPause()
+    {
+        super.onPause();
+        Log.d(tag, "In the onPause() event");
+    }
+    public void onStop()
+    {
+        super.onStop();
+        Log.d(tag, "In the onStop() event");
+    }
+    public void onDestroy()
+    {
+        super.onDestroy();
+        Log.d(tag, "In the onDestroy() event");
+    }
+}
